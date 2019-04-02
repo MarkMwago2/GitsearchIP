73de869cff0d0e0c497c448be5819e75fb1f3702
MarkMwago2 <markmwago@gmail.com>
2019-04-02 18:30:45 +0300

    add import on search

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Repository} from '../repository'
+import {User} from '../user'
 
 @Component({
   selector: 'app-search',
@@ -6,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
-
+  user:any=[];
+  repository:any=[];
+  username:string;
   constructor() { }
 
   ngOnInit() {
