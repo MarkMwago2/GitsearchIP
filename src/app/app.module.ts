caa087f1f5a0b554b3c071dde98194b56beed00b
MarkMwago2 <markmwago@gmail.com>
2019-04-02 18:20:47 +0300

    generate components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+import { IsComponent } from './is/is.component';
+import { ProfileComponent } from './profile/profile.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    IsComponent,
+    ProfileComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule
