d1c39ba2ef5e5cb7994866c4c70a4c75c6f7be20
MarkMwago2 <markmwago@gmail.com>
2019-04-02 18:25:55 +0300

    add function

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {ProfileService} from '../profile.service';
+import { map } from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-profile',
@@ -6,10 +9,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
+  user:any=[];
+  repository:any=[];
+  username:string;
 
-  constructor() { }
+  constructor(private ProfileService: ProfileService) {
+    this.ProfileService.getUser().subscribe(user => {
+      console.log(user);
+
+      this.user = user;
+    })
+    this.ProfileService.getRepository().subscribe(repository => {
+    // console.log(repos)
+      this.repository = repository;
+    })
+  }
 
   ngOnInit() {
   }
+   searchUser(){
+     this.ProfileService.updateUser(this.username);
+     this.ProfileService.getUser().subscribe(user => {
+       this.user = user;
+     })
 
+     this.ProfileService.getRepository().subscribe(repository => {
+
+       this.repository = repository;
+     })
+
+   }
 }
