a29ede6d0fd2a65c5b4aba01c5d189493c1d5262
MarkMwago2 <markmwago@gmail.com>
2019-04-02 18:41:22 +0300

    add changes on app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
+import {HttpClient} from  '@angular/common/http';
+import {ProfileService} from './profile.service';
+// import {SearchComponent} from './profile/profile.component';
+
+
+import {Repository} from './repository';
+import {User} from './user';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers : [ProfileService]
 })
 export class AppComponent {
   title = 'GitsearchIP';
