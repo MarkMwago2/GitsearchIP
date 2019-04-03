f8753a38e6e60c30704090e1d0cf06e207738166
MarkMwago2 <markmwago@gmail.com>
2019-04-03 15:18:50 +0300

    add views on service

--- src/app/profiles/profile.service.ts
+++ src/app/profiles/profile.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
+import {HttpClient}  from '@angular/common/http';
+import { map } from 'rxjs/operators';
+
+
+
+import {Repository} from './repository'
+import {User} from './user'
+import {environment} from '../environments/environment'
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProfileService {
+  username:string;
 
-  constructor() { }
+    constructor(private http: HttpClient){
+      console.log('Github Service Ready...');
+      this.username = 'MarkMwago2';
+
+    }
+    getUser(){
+      return this.http.get("https://api.github.com/users/"+this.username)
+      .pipe(map(result=>result))
+
+    }
+    getRepository(){
+      return this.http.get("https://api.github.com/users/"+this.username +'/repos')
+      .pipe(map(result=>result))
+  }
+  updateUser(username:string){
+    this.username = username;
+  }
 }
